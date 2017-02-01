a038e1428f2ef2e0b5d99a7f118484dae323751d
Douglas Webster <douglas.webster2107@gmail.com>
2017-02-01 12:13:45 +0000

    Tidied up the topnav component.

--- src/app/shared/topnav/topnav.module.ts
+++ src/app/shared/topnav/topnav.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { TopnavComponent } from './topnav.component';
 import { RouterModule } from '@angular/router';
 import { DropdownModule } from 'ng2-bootstrap/dropdown';
+import { CollapseModule } from 'ng2-bootstrap/collapse';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
-    DropdownModule.forRoot()
+    DropdownModule,
+    CollapseModule
   ],
   declarations: [TopnavComponent],
   exports: [TopnavComponent]
