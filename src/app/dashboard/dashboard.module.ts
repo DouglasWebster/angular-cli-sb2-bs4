027fe28adb01f28cc46ba66775a2ff05dedd713b
Douglas Webster <douglas.webster2107@gmail.com>
2017-02-13 10:56:17 +0000

    Moved ng-bootstrap elements to bs-components

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,14 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 
-import { AccordionModule } from 'ng2-bootstrap/accordion';
-import { AlertModule } from 'ng2-bootstrap/alert';
-import { CarouselModule } from 'ng2-bootstrap/carousel';
-import { CollapseModule } from 'ng2-bootstrap/collapse';
-import { DropdownModule } from 'ng2-bootstrap/dropdown';
-import { ModalModule } from 'ng2-bootstrap/modal';
-
-
 import { SidebarModule } from '../shared/sidebar/sidebar.module';
 import { TopnavModule } from '../shared/topnav/topnav.module';
 import { HomeModule } from './home/home.module';
@@ -27,11 +19,6 @@ import { DashboardComponent } from './dashboard.component';
   imports: [
     CommonModule,
     DashboardRoutingModule,
-    AccordionModule.forRoot(),
-    AlertModule.forRoot(),
-    CarouselModule.forRoot(),
-    CollapseModule.forRoot(),
-    DropdownModule.forRoot(),
     SidebarModule,
     TopnavModule,
     HomeModule,
@@ -43,7 +30,7 @@ import { DashboardComponent } from './dashboard.component';
     BsComponentModule
   ],
   declarations: [
-    DashboardComponent
+    DashboardComponent,
   ]
 })
 export class DashboardModule { }
