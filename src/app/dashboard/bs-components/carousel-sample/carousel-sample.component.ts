97551f66038a4b9700101df9bd095c54567a4fd3
DouglasWebster <douglas.webster2107@gmail.com>
2017-02-15 18:32:09 +0000

    Added dynamic slides

--- src/app/dashboard/bs-components/carousel-sample/carousel-sample.component.ts
+++ src/app/dashboard/bs-components/carousel-sample/carousel-sample.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarouselSampleComponent implements OnInit {
 
-  constructor() { }
+  public myInterval = 1500;
+  public slides: any[] = [];
+  public activeSlideIndex: number;
+  public noWrapSlides = false;
+
+  public constructor() {
+    for (let i = 0; i < 4; i++) {
+      this.addSlide();
+    }
+  }
 
   ngOnInit() {
   }
 
+  public addSlide(): void {
+    this.slides.push({
+      image: `assets/img/slider${this.slides.length % 4}.jpg`
+    });
+  }
+
+  public removeSlide(index?: number): void {
+    const toRemove = index ? index : this.activeSlideIndex;
+    this.slides.splice(toRemove, 1);
+  }
 }
