c86c3a50cd85090aa3a68d286a32ce7aed9c849f
DouglasWebster <douglas.webster2107@gmail.com>
2017-03-10 14:30:45 +0000

    Updated to ng-cli-rc.1 and added further tests

--- src/app/dashboard/bs-components/dropdown-sample/dropdown-sample.component.spec.ts
+++ src/app/dashboard/bs-components/dropdown-sample/dropdown-sample.component.spec.ts
@@ -8,21 +8,54 @@ import { DropdownSampleComponent } from './dropdown-sample.component';
 describe('DropdownSampleComponent', () => {
   let component: DropdownSampleComponent;
   let fixture: ComponentFixture<DropdownSampleComponent>;
+  let debugEl: DebugElement;
+  let nativeEl: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DropdownSampleComponent ]
+      declarations: [DropdownSampleComponent]
     });
-   // .compileComponents(); <- not required for webpack
+    // .compileComponents(); <- not required for webpack
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DropdownSampleComponent);
     component = fixture.componentInstance;
+    debugEl = fixture.debugElement;
+    nativeEl = debugEl.nativeElement;
+
     fixture.detectChanges();
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a button labeled Button Dropdown', () => {
+    let btn;
+    debugEl.queryAll(By.css('.btn')).forEach(el => {
+      if (el.nativeElement.outerText === 'Button dropdown') {
+        btn = el;
+      }
+    });
+
+    expect(btn.nativeElement).toBeDefined('Button Dropdown not defined');
+
+  });
+
+  it('should have a list containg \'Action\'', () => {
+
+    debugEl.queryAll(By.css('.btn')).map(el => {
+      if (el.nativeElement.outerText === 'Button dropdown') {
+        el.nativeElement.click();
+      }
+    });
+    expect(debugEl.query(By.css('.dropdown-item')).nativeElement.textContent).toContain('Action');
+  });
+
+/****************
+ * to-do add test to see if the dropdown is opening
+ * and closing when a click event occurs
+*****************/
+
 });
--- src/app/dashboard/bs-components/modal-sample/modal-sample.component.spec.ts
+++ src/app/dashboard/bs-components/modal-sample/modal-sample.component.spec.ts
@@ -14,19 +14,64 @@ describe('ModalSampleComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [ModalModule.forRoot()],
-      declarations: [ ModalSampleComponent ]
+      declarations: [ModalSampleComponent]
     });
-   // .compileComponents(); <- not required for webpack
+    // .compileComponents(); <- not required for webpack
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ModalSampleComponent);
     component = fixture.componentInstance;
-    debugEl =  fixture.debugElement;
+    debugEl = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a button \'child modal\'', () => {
+    const btn = debugEl.queryAll(By.css('.btn')).find(b => {
+      if (b.nativeElement.textContent === 'Open child modal') {
+        return true;
+      }
+    });
+
+    expect(btn.nativeElement).toBeDefined('Open child modal button not defined');
+
+  });
+
+  it('should open the child modal window when the \'child modal\' buttton is pressed ', () => {
+    const btn = debugEl.queryAll(By.css('.btn')).find(b => {
+      if (b.nativeElement.textContent === 'Open child modal') {
+        return true;
+      }
+    });
+
+    // test code coverage.
+    btn.nativeElement.click();
+
+    // then check the call.
+    const spy = spyOn(component, 'showChildModal').and.returnValue(true);
+
+    btn.nativeElement.click();
+    expect(component.showChildModal).toHaveBeenCalled();
+  });
+
+  it('should close the child modal window when a second click occurs', () => {
+    const btn = debugEl.queryAll(By.css('.btn')).find(b => {
+      if (b.nativeElement.textContent === 'Open child modal') {
+        return true;
+      }
+    });
+    
+    btn.nativeElement.click();
+    component.hideChildModal();
+
+    const spy = spyOn(component, 'hideChildModal').and.returnValue(true);
+    component.hideChildModal();
+    expect(component.hideChildModal).toHaveBeenCalled();
+  });
+
+
 });
