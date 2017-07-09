f4be702bab05aa5385240c5e5cd84d84a8f067a2
Douglas Webster <douglas.webster2107@gmail.com>
2017-07-09 12:19:56 +0100

    Updated to latest releases

--- vendor.ts
+++ vendor.ts
@@ -1,13 +1,13 @@
 /*********************
- * 
+ *
  * This is needed to get moment.js working.
- * This file is included in the angular-cli.json 
+ * This file is included in the angular-cli.json
  * file in the scripts section.
  * See https://github.com/angular/angular-cli/issues/2141
- * for a further explanation/reason.  
- * Also import * as moment from 'moment' needs to be 
+ * for a further explanation/reason.
+ * Also import * as moment from 'moment' needs to be
  * imported into the code as well.
- * 
+ *
  ********************/
 
 import * as moment from 'moment';
