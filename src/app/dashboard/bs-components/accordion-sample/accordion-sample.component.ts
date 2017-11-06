443fec5daaf6865196b47cd11ec022a541e64eb4
Douglas Webster <douglas.webster2107@gmail.com>
2017-11-06 15:14:54 +0000

    Updated to match ngx-bootstrap accordian class update

--- src/app/dashboard/bs-components/accordion-sample/accordion-sample.component.ts
+++ src/app/dashboard/bs-components/accordion-sample/accordion-sample.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accordion-sample.component.scss']
 })
 export class AccordionSampleComponent implements OnInit {
+  customClass = 'customClass';
+  isFirstOpen = true;
 
   constructor() { }
 
