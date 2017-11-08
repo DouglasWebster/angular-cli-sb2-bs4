e1390cca1d5a5f586cc46575a5737e8005ca0c07
Douglas Webster <douglas.webster2107@gmail.com>
2017-11-08 15:51:49 +0000

    Changed order of declarations to avoid linting errors

--- src/app/dashboard/bs-components/modal-sample/modal-sample.component.ts
+++ src/app/dashboard/bs-components/modal-sample/modal-sample.component.ts
@@ -2,6 +2,33 @@ import { Component, OnInit, TemplateRef } from '@angular/core';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 
+/* This is a component which we pass in modal*/
+
+@Component({
+  selector: 'app-modal-content',
+  template: `
+    <div class="modal-header">
+      <h4 class="modal-title pull-left">{{title}}</h4>
+      <button type="button" class="close pull-right" aria-label="Close" (click)="bsModalRef.hide()">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <div class="modal-body">
+      <ul *ngIf="list.length">
+        <li *ngFor="let item of list">{{item}}</li>
+      </ul>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-default" (click)="bsModalRef.hide()">Close</button>
+    </div>
+  `
+})
+export class ModalContentComponent {
+  title: string;
+  list: any[] = [];
+  constructor(public bsModalRef: BsModalRef) { }
+}
+
 @Component({
   selector: 'app-modal-sample',
   templateUrl: './modal-sample.component.html',
@@ -42,31 +69,4 @@ export class ModalSampleComponent implements OnInit {
   // public hideChildModal(): void {
   //   this.childModal.hide();
   // }
-}
-
-/* This is a component which we pass in modal*/
-
-@Component({
-  selector: 'app-modal-content',
-  template: `
-    <div class="modal-header">
-      <h4 class="modal-title pull-left">{{title}}</h4>
-      <button type="button" class="close pull-right" aria-label="Close" (click)="bsModalRef.hide()">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
-    <div class="modal-body">
-      <ul *ngIf="list.length">
-        <li *ngFor="let item of list">{{item}}</li>
-      </ul>
-    </div>
-    <div class="modal-footer">
-      <button type="button" class="btn btn-default" (click)="bsModalRef.hide()">Close</button>
-    </div>
-  `
-})
-export class ModalContentComponent {
-  title: string;
-  list: any[] = [];
-  constructor(public bsModalRef: BsModalRef) { }
 }
