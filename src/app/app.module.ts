9c4b83b5d879b3225cedf603f5120c2322fe3585
Douglas Webster <douglas.webster2107@gmail.com>
2017-12-14 20:19:42 +0000

    Substituted HttpClientModule for deprecated HttpModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 // import { LoginModule } from './login/login.module';
 import { AppComponent } from './app.component';
@@ -15,7 +15,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
