5176fdb6c08c81c001bdeeaefc7bdcfd874bed52
Douglas Webster <douglas.webster2107@gmail.com>
2018-04-21 16:46:37 +0100

    Changed to match @ng/cli V1.7.4 setup files

--- karma.conf.js
+++ karma.conf.js
@@ -8,22 +8,12 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      // require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    files: [
-      { pattern: './src/test.ts', watched: false }
-    ],
-    preprocessors: {
-      './src/test.ts': ['@angular/cli']
-    },
-    mime: {
-      'text/x-typescript': ['ts','tsx']
     },
     coverageIstanbulReporter: {
       reports: [ 'html', 'lcovonly' ],
