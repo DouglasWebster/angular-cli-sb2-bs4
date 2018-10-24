48c9555a136a9efb9aa026e3dcd482a6a37582fe
Douglas Webster <douglas.webster2107@gmail.com>
2018-10-24 15:01:36 +0100

    Corrected setup for circleci

---
+++ protractor-ci.conf.js
@@ -0,0 +1,10 @@
+const config = require('./e2e/protractor.conf').config;
+
+config.capabilities = {
+  browserName: 'chrome',
+  chromeOptions: {
+    args: ['--headless', '--no-sandbox']
+  }
+};
+
+exports.config = config;
